18384736f3774604bb6c319bd10065cb332e35bb
SebastianMejia1 <112210351+SebastianMejia1@users.noreply.github.com>
2022-10-28 07:52:27 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -12,10 +12,30 @@ espacio entre palabra y palabra.
 import pandas as pd
 
 
-def ingest_data():
+def clean_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+
+    df.drop(columns= df.columns[0], inplace=True)
+
+    for i in ['tipo_de_emprendimiento' , "barrio" ,'comuna_ciudadano']:
+        df[i] = df[i].fillna(df[i].mode())
+
+   #
+
+    variables_categ = ['sexo','tipo_de_emprendimiento','idea_negocio','barrio', df.columns[-1]]
+    for i in variables_categ:
+        df[i] = df[i].str.lower()
+
+    variables_num = ['estrato','comuna_ciudadano','monto_del_credito']
+    for i in variables_num:
+        df[i] = pd.to_numeric (df[i],errors= 'coerce')
+
+    df['monto_del_credito'] = df['monto_del_credito'].fillna(df['monto_del_credito'].mean())
+
+    df.drop_duplicates(keep = 'last', inplace= True)
+ 
 
     return df
+
+  
