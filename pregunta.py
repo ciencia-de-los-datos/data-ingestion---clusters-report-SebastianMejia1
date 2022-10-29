db1368e1e497ba0085470f95e56a8b89fe269f1f
SebastianMejia1 <112210351+SebastianMejia1@users.noreply.github.com>
2022-10-28 21:58:16 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,28 +1,30 @@
 """
 Ingestión de datos - Reporte de clusteres
 -----------------------------------------------------------------------------------------
-
 Construya un dataframe de Pandas a partir del archivo 'clusters_report.txt', teniendo en
 cuenta que los nombres de las columnas deben ser en minusculas, reemplazando los espacios
 por guiones bajos; y que las palabras clave deben estar separadas por coma y con un solo 
 espacio entre palabra y palabra.
-
-
 """
 import pandas as pd
 
+
+
 def ingest_data():
 
-    datos=pd.read_table("clusters_report.txt",header=None)
+    data=pd.read_table("clusters_report.txt",header=None)
+    
+
+    
+    data.drop([ 0,1,2],axis=0, inplace=True)
     i=0
-    datos.drop([0,1,2],axis=0, inplace=True)
-    Rta=[]
+    rta=[]
     fila = {}
-    for i in range(len(datos)):
-        line=Rta.iloc[i][0].strip().split()
+    for i in range(len(data)):
+        line=data.iloc[i][0].strip().split()
         if line[0].isnumeric():
             if len(fila)>0:
-                Rta.append(fila)
+                rta.append(fila)
             fila={}
             fila["cluster"]=int(line[0])
             fila["cantidad_de_palabras_clave"]=int(line[1])
@@ -33,10 +35,9 @@ def ingest_data():
             if fila["principales_palabras_clave"][-1]==".":
                 fila["principales_palabras_clave"]=fila["principales_palabras_clave"][:-1]
     if len(fila) > 0:
-        Rta.append(fila)
-    datos=pd.DataFrame(Rta)
-    return datos
-
+        rta.append(fila)
+    data=pd.DataFrame(rta)
+    return data
 
 
 
