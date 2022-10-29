8e727fbd628262d70c78b9ed527e34d3993e1af1
SebastianMejia1 <112210351+SebastianMejia1@users.noreply.github.com>
2022-10-28 19:04:08 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,6 +11,32 @@ espacio entre palabra y palabra.
 """
 import pandas as pd
 
+def ingest_data():
+
+    datos=pd.read_table("clusters_report.txt",header=None)
+    j=0
+    datos.drop([0,1,2],axis=0, inplace=True)
+    Rta=[]
+    fila = {}
+    for j in range(len(df)):
+        line=Rta.iloc[j][0].strip().split()
+        if line[0].isnumeric():
+            if len(fila)>0:
+                Rta.append(fila)
+            fila={}
+            fila["cluster"]=int(line[0])
+            fila["cantidad_de_palabras_clave"]=int(line[1])
+            fila["porcentaje_de_palabras_clave"]=float(line[2].replace(",","."))
+            fila["principales_palabras_clave"]=" ".join(line[4:])
+        else:
+            fila["principales_palabras_clave"]+=" "+" ".join(line)
+            if fila["principales_palabras_clave"][-1]==".":
+                fila["principales_palabras_clave"]=fila["principales_palabras_clave"][:-1]
+    if len(fila) > 0:
+        Rta.append(fila)
+    df=pd.DataFrame(Rta)
+    return datos
+
 
 
 
